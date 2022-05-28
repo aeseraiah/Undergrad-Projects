7e318b2cb72c0263295171c12777eb3cc5be7931
Seraiah, Anna Elisabeth <aeseraiah@gmail.com>
2022-05-28 14:59:27 -0400

    update

---
+++ python_practice/conditionals.py
@@ -0,0 +1,22 @@
+
+#the int function tells python to convert the input to an integer value.
+#the default value is a string, so if int was not used, the print line would concatenate the two numbers instead of adding them
+x = int(input("What's x "))
+y = int(input("What's y "))
+
+print(x+y)
+
+if x < y: #boolean logic. If this statement is true, statement is printed
+    print("x is less than y")
+
+elif x > y: 
+    print("x is greater than y")
+
+else: #else doesn't include a conditional parameter
+    print("x is equal to y")
+
+
+if x != y:
+    print("x is not equal to y")
+else: 
+    print("x is equal to y")
---
+++ python_practice/functions.py
@@ -0,0 +1,14 @@
+def main():
+    name = input("What's your name? ")
+    example(name)
+
+
+
+def example(x="world"): #including a default parameter for x
+    print("hello,", x)
+
+main()
+
+
+
+
