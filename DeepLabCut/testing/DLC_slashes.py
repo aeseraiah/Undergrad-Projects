06cbc5436a3c4f35ac0eb84c09e8f98ba9d8539d
Seraiah, Anna Elisabeth <aeseraiah@gmail.com>
2022-06-28 14:49:27 -0400

    update

---
+++ DeepLabCut/testing/DLC_slashes.py
@@ -0,0 +1,15 @@
+from google.colab import drive
+import pandas as pd
+import glob
+import os
+
+
+csvfile = pd.read_csv('CollectedData_jic.csv')
+
+#replacing all forward slashes with back slashes in path
+csvfile['scorer'] = csvfile['scorer'].str.replace('/', '\\', regex=True) 
+corrected_csvfile = csvfile.copy()
+
+#pandas doesn't allow duplicate column names when creating csv file
+new = corrected_csvfile.rename(columns = {'jic':'jic', 'jic.1':'jic', 'jic.2':'jic', 'jic.3':'jic', 'jic.3':'jic', 'jic.4':'jic', 'jic.5':'jic', 'jic.6':'jic', 'jic.7':'jic',}, inplace=False)
+finalcsv = new.to_csv('CollectedData_jic.csv', index=False)
