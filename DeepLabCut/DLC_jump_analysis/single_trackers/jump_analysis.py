943fc9b4c1b853e7511f7bff5ca9e5bf4f7d6454
Anna <aeseraiah@gmail.com>
2022-08-05 20:32:48 -0400

    DLC update

--- DeepLabCut/DLC_jump_analysis/single_trackers/jump_analysis.py
+++ DeepLabCut/DLC_jump_analysis/single_trackers/jump_analysis.py
@@ -1,59 +1,97 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-nose_file = 'jump_nose.csv'
-rump_file = 'jump_rump.csv'
-
-def main(nose_csv, rump_csv):
-    nose = pd.read_csv(nose_csv)
-    nose_df = pd.DataFrame(nose)
-    nose_values = nose_df.columns.values.astype(float)
-    #print("nose_values:\n", nose_values)
-    Ms22_values = nose_values[0:5]
-    print("Ms22_values:\n", Ms22_values)
-    Rear5_values = nose_values[5:10]
-    print("Rear5_values:\n", Rear5_values)
-
-    rump = pd.read_csv(rump_csv)
-    rump_df = pd.DataFrame(rump)
-    rump_values = rump_df.columns.values.astype(float)
-    #print("rump_values:\n", rump_values)
+single_tracker_nose_csv = 'single-tracker_jump-nose.csv'
+single_tracker_rump_csv = 'single-tracker_jump-rump.csv'
 
 
+nose_csv_list = [single_tracker_nose_csv]
+rump_csv_list = [single_tracker_rump_csv]
 
 
-    w = 0.1  # bar width
-    x = [1] # x-coordinates of your bars
-    colors = [(0, 0, 1, 1), (1, 0, 0, 1)]    # corresponding colors
-    y2 = Ms22_values#, Rear5_values
-    #y = [Ms22_values,Rear5_values]
-    y = [nose_values]
-    print(y)
+def main(single_tracker_nose_csv, single_tracker_rump_csv):
+    # for i in nose_csv_list:
+    #     nose = pd.read_csv(i)
+    #     nose_df = pd.DataFrame(nose)
+    #     nose_values = nose_df.columns.values.astype(float)
+
+    nose_directory = 'csv_files'
+    #NOTE: change nose_directory in for loop to rump_directory to make the other bar graph
+    rump_directory = 'csv_rump_files'
+    
+    # iterate over files in
+    # that directory
+    arr = []
+    for filename in os.listdir(nose_directory):
+        f = os.path.join(nose_directory, filename)
+
+        nose = pd.read_csv(f)
+        nose_df = pd.DataFrame(nose)
+        nose_values = nose_df.columns.values.astype(float)
+        print(nose_values)
+        for i in nose_values:
+            arr[i] = nose_values
+    print(arr)
+
+
+
+
+
+main(nose_csv_list, rump_csv_list)
+
+
+
+    # nose = pd.read_csv(single_tracker_nose_csv)
+    # nose_df = pd.DataFrame(nose)
+    # nose_values = nose_df.columns.values.astype(float)
+    # #print("nose_values:\n", nose_values)
+    # Ms22_values = nose_values[0:5]
+    # print("Ms22_values:\n", Ms22_values)
+    # Rear5_values = nose_values[5:10]
+    # print("Rear5_values:\n", Rear5_values)
+
+    # rump = pd.read_csv(single_tracker_rump_csv)
+    # rump_df = pd.DataFrame(rump)
+    # rump_values = rump_df.columns.values.astype(float)
+    # #print("rump_values:\n", rump_values)
+
+
+
+
+    # w = 0.1  # bar width
+    # x = [1] # x-coordinates of your bars
+    # colors = [(0, 0, 1, 1), (1, 0, 0, 1)]    # corresponding colors
+    # y2 = Ms22_values#, Rear5_values
+    # #y = [Ms22_values,Rear5_values]
+    # y = [nose_values]
+    # print(y)
  
 
-    fig, ax = plt.subplots()
-    ax.bar(x,
-           height=[np.mean(yi) for yi in y],
-           yerr=[np.std(yi) for yi in y],    # error bars
-           capsize=12, # error bar cap width in points
-           width=w,    # bar width
-           tick_label=["Rear5/Ms22", "Combined", "Ms21"],#,"RC1"],
-           color=(0,0,0,0),  # face color transparent
-           edgecolor=colors,
-         #ecolor=colors,    # error bar colors; setting this raises an error for whatever reason.
-         )
+    # fig, ax = plt.subplots()
+    # ax.bar(x,
+    #        height=[np.mean(yi) for yi in y],
+    #        yerr=[np.std(yi) for yi in y],    # error bars
+    #        capsize=12, # error bar cap width in points
+    #        width=w,    # bar width
+    #        tick_label=["Rear5/Ms22","Ms21"],#,"Combined", "RC1"],
+    #        color=(0,0,0,0),  # face color transparent
+    #        edgecolor=colors,
+    #      #ecolor=colors,    # error bar colors; setting this raises an error for whatever reason.
+    #      )
 
-    for i in range(len(x)):
-        #distribute scatter randomly across whole width of bar
-        a = y[i]
-        ax.scatter(x[i] + np.random.random(a[0:5].size) * w - w / 2, a[0:5], color=colors[i])
-        ax.scatter(x[i] + np.random.random(a[5:10].size) * w - w / 2, a[5:10], color=colors[i+1])
-        print(y[0])
+    # for i in range(len(x)):
+    #     #distribute scatter randomly across whole width of bar
+    #     a = y[i]
+    #     ax.scatter(x[i] + np.random.random(a[0:5].size) * w - w / 2, a[0:5], color=colors[i])
+    #     ax.scatter(x[i] + np.random.random(a[5:10].size) * w - w / 2, a[5:10], color=colors[i+1])
+    #     print(y[0])
 
-    plt.show()
+    # plt.show()
+    # #plt.save()
 
 
 
 
-main(nose_file, rump_file)
+#main(nose_file, rump_file)
