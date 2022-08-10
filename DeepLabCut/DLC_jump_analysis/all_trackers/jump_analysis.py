ed7638c87fd5d6ae3b168906be81b74dfc24ca84
Anna <aeseraiah@gmail.com>
2022-08-10 16:01:42 -0400

    DLC udpate

--- DeepLabCut/DLC_jump_analysis/all_trackers/jump_analysis.py
+++ DeepLabCut/DLC_jump_analysis/all_trackers/jump_analysis.py
@@ -5,8 +5,6 @@ from pathlib import Path
 import numpy as np
 import os
 
-
-
 full_path_directory = "C:/Users/7teal/Coding Projects/Undergrad-Projects/DeepLabCut/DLC_jump_analysis/all_trackers/"
 nose_directory = "csv_files"
 rump_directory = "csv_rump_files"
@@ -32,18 +30,19 @@ def main(full_path, relative_path, fname):
 
     combined_new_df_values = dataframes_list[0]
     Ms21_new_df_values = dataframes_list[1]
-    #Order of single trackers: Ms22, RC1, Rear5
-    Ms22_RC1_Rear5_new_df_values = dataframes_list[2]
-    print(Ms22_RC1_Rear5_new_df_values)
+    #Order of single trackers: Ms21, Ms22, RC1, Rear5
+    single_trackers_df = dataframes_list[2]
     #RC1_new_df_values = dataframes_list[3]
     #AVG THE VALUES FROM 4 RATS
   
 
     w = .9 # bar width
-    x = [1, 2, 3] # x-coordinates of your bars
-    colors = ['blue', 'red', 'green']  # corresponding colors
-    #y = Ms21_df_values, Ms21_new_df_values, Ms22_Rear5_df_values, Ms22_Rear5_new_df_values, combined_df_values, combined_new_df_values
-    y = Ms22_RC1_Rear5_new_df_values, Ms21_new_df_values, combined_new_df_values
+    #x = [1, 2, 3] # x-coordinates of your bars
+    x = [1, 2]
+    #colors = ['blue', 'red', 'green']  # corresponding colors
+    colors = ['blue','red']
+    #y = Ms22_RC1_Rear5_new_df_values, Ms21_new_df_values, combined_new_df_values
+    y = single_trackers_df, single_trackers_df
 
     fig, ax = plt.subplots()
     ax.bar(x,
@@ -51,7 +50,8 @@ def main(full_path, relative_path, fname):
            yerr=[np.std(yi) for yi in y],    # error bars
            capsize=12, # error bar cap width in points
            width=w,    # bar width
-           tick_label=["Rear5/Ms22", "Ms21", "combined"],#"RC1"],
+           #tick_label=["Rear5/Ms22", "Ms21", "combined"],#"RC1"],
+           tick_label=['single trackers', 'test'],
            color=(0,0,0,0),  # face color transparent
            edgecolor=colors,
          #ecolor=colors,    # error bar colors; setting this raises an error for whatever reason.
@@ -60,10 +60,11 @@ def main(full_path, relative_path, fname):
     for i in range(len(x)):
         #distribute scatter randomly across whole width of bar
         a = y[i]
-        ax.scatter(x[i] + np.random.random(a[0:5].size) * w - w / 2, a[0:5], color=colors[i], marker='.')
-        ax.scatter(x[i] + np.random.random(a[5:10].size) * w - w / 2, a[5:10], color=colors[i], marker = "s")
-        ax.scatter(x[i] + np.random.random(a[5:10].size) * w - w / 2, a[10:15], color=colors[i], marker = "t")
-        ax.scatter(x[i] + np.random.random(a[5:10].size) * w - w / 2, a[15:20], color=colors[i], marker = "|")
+        print(y)
+        ax.scatter(x[i] + np.random.random(a[0:5].size) * w - w / 2, a[0:5], color=colors[i], marker='.') #circle marker = Ms21
+        ax.scatter(x[i] + np.random.random(a[5:10].size) * w - w / 2, a[5:10], color=colors[i], marker = "s") #square marker = Ms22
+        ax.scatter(x[i] + np.random.random(a[10:15].size) * w - w / 2, a[10:15], color=colors[i], marker = "*") #star marker = RC1
+        ax.scatter(x[i] + np.random.random(a[15:20].size) * w - w / 2, a[15:20], color=colors[i], marker = "^") #triangle marker = Rear5
 
     # box = ax.get_position()
     # ax.set_position([box.x0, box.y0 + box.height * 0.1,
