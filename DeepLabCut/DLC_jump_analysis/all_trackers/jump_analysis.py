090f89fb79488a8a3abfbcf3d4b1e577e9eba676
Anna <aeseraiah@gmail.com>
2022-08-19 19:45:08 -0400

    DLC update

--- DeepLabCut/DLC_jump_analysis/all_trackers/jump_analysis.py
+++ DeepLabCut/DLC_jump_analysis/all_trackers/jump_analysis.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import numpy as np
 import os
 
-#shows 3 graphs in total. First: single, combined. Second: 4 individual trackers. Third: single, avg of 4 ind trackers, combined
+#shows 3 graphs in total. First: single, combined. Second: 4 individual trackers. Third: single, combined, avg of 4 ind trackers
 
 full_path_directory = "C:/Users/7teal/Coding Projects/Undergrad-Projects/DeepLabCut/DLC_jump_analysis/all_trackers/"
 org_nose_directory = "org_csv_nose_files"
@@ -15,7 +15,7 @@ nose_directory = "csv_nose_files"
 rump_directory = "csv_rump_files"
 
 
-def main(full_path, relative_path, fname1, fname2):
+def main(full_path, relative_path, fname1, fname2, fname3):
     current_dir = os.getcwd()
     os.chdir(current_dir + '/' + relative_path)
 
@@ -34,17 +34,17 @@ def main(full_path, relative_path, fname1, fname2):
         dataframes_list.append(nose_values1)
 
     combined_values = dataframes_list[0]
-    print("combined_values:\n", combined_values)
+    #print("combined_values:\n", combined_values)
     Ms21_values = dataframes_list[1]
-    print("Ms21_values:\n", Ms21_values)
+    #print("Ms21_values:\n", Ms21_values)
     Ms22_values = dataframes_list[2]
-    print("Ms22_values:\n", Ms22_values)
+    #print("Ms22_values:\n", Ms22_values)
     RC1_values = dataframes_list[3]
-    print("RC1_values:\n", RC1_values)
+    #print("RC1_values:\n", RC1_values)
     Rear5_values = dataframes_list[4]
-    print("Rear5_values:\n", Rear5_values)
+    #print("Rear5_values:\n", Rear5_values)
     single_values = dataframes_list[5]
-    print("single_values:\n", single_values)
+    #print("single_values:\n", single_values)
 
     #Order of single trackers: Ms21, Ms22, RC1, Rear5
     #Naming convention: Ms21 = Rat1, Ms22 = Rat2, RC1 = Rat3, Rear5 = Rat4
@@ -56,19 +56,11 @@ def main(full_path, relative_path, fname1, fname2):
     x3 = [1, 2, 3]
     colors = ['red', 'blue']
     y1 = single_values, combined_values
-    y2 = Ms21_values, Ms22_values, RC1_values, Rear5_values 
+    y2 = Ms21_values, Ms22_values, RC1_values, Rear5_values
 
     labels1 = ['single', 'combined']
     labels2 = ['Ms21', 'Ms22', 'RC1', 'Rear5']
-    labels3 = ["single', 'combined', 'individual_avg'"]
-
-    #AVG THE VALUES FROM 3 SINGLE TRACKERS
-    avg_per_tracker =[np.mean(yi) for yi in y2]
-    print(avg_per_tracker)
-    avg_all_trackers = np.mean(avg_per_tracker)
-    print("avg_all_trackers:\n", avg_all_trackers)
-
-    y3 = single_values, combined_values, avg_all_trackers
+    labels3 = ['single', 'combined', 'individual_tracker_avg']
 
 
     figure1 = plt.figure(1)
@@ -84,20 +76,14 @@ def main(full_path, relative_path, fname1, fname2):
     single_combined_bar_val=[np.mean(yi) for yi in y1]
     print("single_combined_bar_val:\n", single_combined_bar_val)
 
-    # exact_values=[np.mean(yi) for yi in y3]
-    # print(exact_values)
-
-    # test_a = y1[0] 
-    # test_var = np.random.random(test_a[0:5].size)
-    # print(test_var)
 
     for i in range(len(x1)):
         #distribute scatter randomly across whole width of bar
         a = y1[i]
-        plt.scatter(x1[i] + np.random.random(a[0:5].size) * w - w / 2, a[0:5], color=colors[1], marker='o') #circle marker = Ms21
-        plt.scatter(x1[i] + np.random.random(a[5:10].size) * w - w / 2, a[5:10], color=colors[1], marker = "s") #square marker = Ms22
-        plt.scatter(x1[i] + np.random.random(a[10:15].size) * w - w / 2, a[10:15], color=colors[0], marker = "*") #star marker = RC1
-        plt.scatter(x1[i] + np.random.random(a[15:20].size) * w - w / 2, a[15:20], color=colors[0], marker = "^") #triangle marker = Rear5
+        plt.scatter(x1[i] + np.array([0.1, 0.3, 0.5, 0.7, 0.9]) * w - w / 2, a[0:5], color=colors[1], marker='o') #circle marker = Ms21
+        plt.scatter(x1[i] + np.array([0.05, 0.25, 0.4, 0.6, 0.8]) * w - w / 2, a[5:10], color=colors[1], marker = "s") #square marker = Ms22
+        plt.scatter(x1[i] + np.array([0.15, 0.4, 0.5, 0.7, 0.9]) * w - w / 2, a[10:15], color=colors[0], marker = "*") #star marker = RC1
+        plt.scatter(x1[i] + np.array([0.1, 0.4, 0.55, 0.7, 0.9]) * w - w / 2, a[15:20], color=colors[0], marker = "^") #triangle marker = Rear5
 
     os.chdir(current_dir + '/saved_figs')
     plt.savefig(fname1, dpi=100)
@@ -116,11 +102,9 @@ def main(full_path, relative_path, fname1, fname2):
            edgecolor=['black'],
          )
 
-    height2=[np.mean(yi) for yi in y2]
-    print(height2)
-    mean_height = [np.mean(height2)]
-    print(mean_height)
-    
+    ind_tracker_values =[np.mean(yi) for yi in y2]
+    ind_trackers_avg = [np.mean(ind_tracker_values)]
+    #print("ind_trackers_avg:\n", ind_trackers_avg)
 
     #csv order for ind trackers:
     #Ms21: Ms22, RC1, Rear5
@@ -133,38 +117,31 @@ def main(full_path, relative_path, fname1, fname2):
     #star marker = RC1
     #triangle marker = Rear5
 
+    Ms21_tracker = y2[0]
+    Ms22_tracker = y2[1]
+    RC1_tracker = y2[2]
+    Rear5_tracker = y2[3]   
 
-    # Ms21 = y2[0]
-    # Ms22 = y2[1]
-    # RC1 = y2[2]
-    # Rear5 = y2[3]
-    # arr_temp = [Ms21, Ms22, RC1, Rear5]
 
-    #for i in range(len(x2)):
-    Ms21 = y2[0]
-    Ms22 = y2[1]
-    RC1 = y2[2]
-    Rear5 = y2[3]
-    #for i in range(len(arr_temp)):
     #Ms21 Tracker:
-    plt.scatter(x2[0] + np.random.random(Ms21[0:5].size) * w - w / 2, Ms21[0:5], color=colors[1], marker='s') #square marker = Ms22
-    plt.scatter(x2[0] + np.random.random(Ms21[5:10].size) * w - w / 2, Ms21[5:10], color=colors[0], marker='*') #star marker = RC1
-    plt.scatter(x2[0] + np.random.random(Ms21[10:15].size) * w - w / 2, Ms21[10:15], color=colors[0], marker='^') #triangle marker = Rear5
+    plt.scatter(x2[0] + np.array([0.05, 0.25, 0.4, 0.6, 0.8]) * w - w / 2, Ms21_tracker[0:5], color=colors[1], marker='s') #square marker = Ms22
+    plt.scatter(x2[0] + np.array([0.12, 0.4, 0.5, 0.7, 0.9]) * w - w / 2, Ms21_tracker[5:10], color=colors[0], marker='*') #star marker = RC1
+    plt.scatter(x2[0] + np.array([0.1, 0.4, 0.55, 0.7, 0.9]) * w - w / 2, Ms21_tracker[10:15], color=colors[0], marker='^') #triangle marker = Rear5
 
     #Ms22 Tracker:
-    plt.scatter(x2[1] + np.random.random(Ms22[0:5].size) * w - w / 2, Ms22[0:5], color=colors[1], marker = "o") #circle marker = Ms21
-    plt.scatter(x2[1] + np.random.random(Ms22[5:10].size) * w - w / 2, Ms22[5:10], color=colors[0], marker = "*") #star marker = RC1
-    plt.scatter(x2[1] + np.random.random(Ms22[10:15].size) * w - w / 2, Ms22[10:15], color=colors[0], marker='^') #triangle marker = Rear5
+    plt.scatter(x2[1] + np.array([0.8, 0.3, 0.5, 0.7, 0.9]) * w - w / 2, Ms22_tracker[0:5], color=colors[1], marker = "o") #circle marker = Ms21
+    plt.scatter(x2[1] + np.array([0.12, 0.4, 0.5, 0.7, 0.9]) * w - w / 2, Ms22_tracker[5:10], color=colors[0], marker = "*") #star marker = RC1
+    plt.scatter(x2[1] + np.array([0.1, 0.4, 0.55, 0.7, 0.9]) * w - w / 2, Ms22_tracker[10:15], color=colors[0], marker='^') #triangle marker = Rear5
 
     #RC1 Tracker:
-    plt.scatter(x2[2] + np.random.random(RC1[0:5].size) * w - w / 2, RC1[0:5], color=colors[1], marker = "o") #circle marker = Ms21
-    plt.scatter(x2[2] + np.random.random(RC1[5:10].size) * w - w / 2, RC1[5:10], color=colors[1], marker = "s") #square marker = Ms22
-    plt.scatter(x2[2] + np.random.random(RC1[10:15].size) * w - w / 2, RC1[10:15], color=colors[0], marker='^') #triangle marker = Rear5      
+    plt.scatter(x2[2] + np.array([0.8, 0.3, 0.5, 0.7, 0.9]) * w - w / 2, RC1_tracker[0:5], color=colors[1], marker = "o") #circle marker = Ms21
+    plt.scatter(x2[2] + np.array([0.05, 0.25, 0.4, 0.6, 0.8]) * w - w / 2, RC1_tracker[5:10], color=colors[1], marker = "s") #square marker = Ms22
+    plt.scatter(x2[2] + np.array([0.1, 0.4, 0.55, 0.7, 0.9]) * w - w / 2, RC1_tracker[10:15], color=colors[0], marker='^') #triangle marker = Rear5      
 
     #Rear5 Tracker:
-    plt.scatter(x2[3] + np.random.random(Rear5[0:5].size) * w - w / 2, Rear5[0:5], color=colors[1], marker = "o") #circle marker = Ms21
-    plt.scatter(x2[3] + np.random.random(Rear5[5:10].size) * w - w / 2, Rear5[5:10], color=colors[1], marker = "s") #square marker = Ms22
-    plt.scatter(x2[3] + np.random.random(Rear5[10:15].size) * w - w / 2, Rear5[10:15], color=colors[0], marker='*') #star marker = RC1
+    plt.scatter(x2[3] + np.array([0.8, 0.3, 0.5, 0.7, 0.9]) * w - w / 2, Rear5_tracker[0:5], color=colors[1], marker = "o") #circle marker = Ms21
+    plt.scatter(x2[3] + np.array([0.05, 0.25, 0.4, 0.6, 0.8]) * w - w / 2, Rear5_tracker[5:10], color=colors[1], marker = "s") #square marker = Ms22
+    plt.scatter(x2[3] + np.array([0.12, 0.4, 0.5, 0.7, 0.9]) * w - w / 2, Rear5_tracker[10:15], color=colors[0], marker='*') #star marker = RC1
 
   
     plt.savefig(fname2, dpi=100)
@@ -172,21 +149,88 @@ def main(full_path, relative_path, fname1, fname2):
     plt.close()
 
 
-    #SINGLE, COMBINED, AND AVG GRAPH: 
-    # figure3 = plt.figure(1)
-    # plt.bar(x3,
-    #     height=[np.mean(yi) for yi in y1],
-    #     #yerr=[np.std(yi) for yi in y1],    # error bars
-    #     capsize=12, # error bar cap width in points
-    #     width=w,    # bar width
-    #     tick_label=labels3,
-    #     color=(0,0,0,0),  # face color transparent
-    #     edgecolor=['black'],
-    #         )
+    #FINDING AVERAGES OF 5 VIDEOS (FROM EACH ANIMAL) ANALYZED WITH ALL 4 INDIVIDUAL TRACKERS:
 
-    # plt.savefig('test_graph.png', dpi=100)
-    # plt.show()
-    # plt.close()
+    #MS21 VIDEOS:
+    Ms21_first_vid = (np.mean(Ms22_tracker[0]) + np.mean(RC1_tracker[0]) + np.mean(Rear5_tracker[0])) / 3
+    Ms21_second_vid = (np.mean(Ms22_tracker[1]) + np.mean(RC1_tracker[1]) + np.mean(Rear5_tracker[1])) / 3
+    Ms21_third_vid = (np.mean(Ms22_tracker[2]) + np.mean(RC1_tracker[2]) + np.mean(Rear5_tracker[2])) / 3
+    Ms21_fourth_vid = (np.mean(Ms22_tracker[3]) + np.mean(RC1_tracker[3]) + np.mean(Rear5_tracker[3])) / 3
+    Ms21_fifth_vid = (np.mean(Ms22_tracker[4]) + np.mean(RC1_tracker[4]) + np.mean(Rear5_tracker[4])) / 3
+
+    Ms21_avg_five_vids = [Ms21_first_vid,  Ms21_second_vid, Ms21_third_vid, Ms21_fourth_vid, Ms21_fifth_vid]
+    #print("Ms21_avg_five_vids:\n", Ms21_avg_five_vids)
+
+
+    #MS22 VIDEOS:
+    Ms22_first_vid = (np.mean(Ms21_tracker[0]) + np.mean(RC1_tracker[6]) + np.mean(Rear5_tracker[6])) / 3
+    Ms22_second_vid = (np.mean(Ms21_tracker[1]) + np.mean(RC1_tracker[7]) + np.mean(Rear5_tracker[7])) / 3
+    Ms22_third_vid = (np.mean(Ms21_tracker[2]) + np.mean(RC1_tracker[8]) + np.mean(Rear5_tracker[8])) / 3
+    Ms22_fourth_vid = (np.mean(Ms21_tracker[3]) + np.mean(RC1_tracker[9]) + np.mean(Rear5_tracker[9])) / 3
+    Ms22_fifth_vid = (np.mean(Ms21_tracker[4]) + np.mean(RC1_tracker[10]) + np.mean(Rear5_tracker[10])) / 3
+
+    Ms22_avg_five_vids = [Ms22_first_vid,  Ms22_second_vid, Ms22_third_vid, Ms22_fourth_vid, Ms22_fifth_vid]
+    #print("Ms22_avg_five_vids:\n", Ms22_avg_five_vids)
+
+    #RC1 VIDEOS:
+    RC1_first_vid = (np.mean(Ms21_tracker[6]) + np.mean(Ms22_tracker[6]) + np.mean(Rear5_tracker[10])) / 3
+    RC1_second_vid = (np.mean(Ms21_tracker[7]) + np.mean(Ms22_tracker[7]) + np.mean(Rear5_tracker[11])) / 3
+    RC1_third_vid = (np.mean(Ms21_tracker[8]) + np.mean(Ms22_tracker[8]) + np.mean(Rear5_tracker[12])) / 3
+    RC1_fourth_vid = (np.mean(Ms21_tracker[9]) + np.mean(Ms22_tracker[9]) + np.mean(Rear5_tracker[13])) / 3
+    RC1_fifth_vid = (np.mean(Ms21_tracker[10]) + np.mean(Ms22_tracker[10]) + np.mean(Rear5_tracker[14])) / 3
+
+    RC1_avg_five_vids = [RC1_first_vid,  RC1_second_vid, RC1_third_vid, RC1_fourth_vid, RC1_fifth_vid]
+    #print("RC1_avg_five_vids:\n", RC1_avg_five_vids)
+
+    #REAR5 VIDEOS:
+    Rear5_first_vid = (np.mean(Ms21_tracker[10]) + np.mean(Ms22_tracker[10]) + np.mean(RC1_tracker[10])) / 3
+    Rear5_second_vid = (np.mean(Ms21_tracker[11]) + np.mean(Ms22_tracker[11]) + np.mean(RC1_tracker[11])) / 3
+    Rear5_third_vid = (np.mean(Ms21_tracker[12]) + np.mean(Ms22_tracker[12]) + np.mean(RC1_tracker[12])) / 3
+    Rear5_fourth_vid = (np.mean(Ms21_tracker[13]) + np.mean(Ms22_tracker[13]) + np.mean(RC1_tracker[13])) / 3
+    Rear5_fifth_vid = (np.mean(Ms21_tracker[14]) + np.mean(Ms22_tracker[14]) + np.mean(RC1_tracker[14])) / 3
+
+    Rear5_avg_five_vids = [Rear5_first_vid,  Rear5_second_vid, Rear5_third_vid, Rear5_fourth_vid, Rear5_fifth_vid]
+    #print("Rear5_avg_five_vids:\n", Rear5_avg_five_vids)
+
+    final_avg_ind_trackers = np.concatenate((Ms21_avg_five_vids, Ms22_avg_five_vids, RC1_avg_five_vids, Rear5_avg_five_vids))
+    #print("final_avg_ind_trackers:\n", final_avg_ind_trackers)
+
+
+
+    #SINGLE, COMBINED, AND AVG GRAPH:
+    y3 = single_values, combined_values, final_avg_ind_trackers
+    figure3 = plt.figure(3)
+    plt.bar(x3,
+        height=[np.mean(yi) for yi in y3],
+        yerr=[np.std(yi) for yi in y3],    # error bars
+        capsize=12, # error bar cap width in points
+        width=w,    # bar width
+        tick_label=labels3,
+        color=(0,0,0,0),  # face color transparent
+        edgecolor=['black'],
+            )
+
+    all_bar_val =[np.mean(final_avg_ind_trackers)]
+    print("\nfinal_avg_ind_trackers:\n", all_bar_val)
+
+    #Aligning videos analyzed on each tracker:
+    # arr = np.random.RandomState(1111)
+    # xb = arr.uniform(0,.95,5)
+    # array = np.array(xb)
+    # array2 = np.sort(array)
+    # print(array2)
+
+    for i in range(len(x3)):
+        #distribute scatter randomly across whole width of bar
+        a = y3[i]
+        plt.scatter(x3[i] + np.array([0.1, 0.3, 0.5, 0.7, 0.9]) * w - w / 2, a[0:5], color=colors[1], marker='o') #circle marker = Ms21
+        plt.scatter(x3[i] + np.array([0.05, 0.25, 0.4, 0.6, 0.8]) * w - w / 2, a[5:10], color=colors[1], marker = "s") #square marker = Ms22
+        plt.scatter(x3[i] + np.array([0.15, 0.4, 0.6, 0.7, 0.9]) * w - w / 2, a[10:15], color=colors[0], marker = "*") #star marker = RC1
+        plt.scatter(x3[i] + np.array([0.1, 0.4, 0.55, 0.7, 0.9]) * w - w / 2, a[15:20], color=colors[0], marker = "^") #triangle marker = Rear5
+
+    plt.savefig(fname3, dpi=200)
+    plt.show()
+    plt.close()
 
     
     #LEGEND:
@@ -230,4 +274,4 @@ def main(full_path, relative_path, fname1, fname2):
     # #plt.legend(['x = x'],['s'],bbox_to_anchor =(0.65, 1.25), loc='lower center')
 
 
-main(full_path_directory, nose_directory, "test-nose-results_single_combined.png", "nose-results_ind.png")#, "nose_results_2.png")
+main(full_path_directory, nose_directory, "test-nose-results_single_combined.png", "nose-results_ind.png", "nose-results-all.png")#, "nose_results_2.png")
